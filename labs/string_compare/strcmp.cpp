7ffa49fee173e63eb0adb911035e5e00aa5952a8
Oleg Silkin <o.silkin98@gmail.com>
2017-09-03 23:31:41 -0500

    Update strcmp.cpp

--- labs/string_compare/strcmp.cpp
+++ labs/string_compare/strcmp.cpp
@@ -3,19 +3,19 @@
 #include "strextra.h"
 #include <stdio.h>
 
-#define sz(x) (sizeof(x)/sizeof(x[0]))
+#define size(x) (sizeof(x)/sizeof(x[0]))
 
 using namespace std;
 
 int main() {
-	char s1[] = "a";
-	char s2[] = "ba";
+	char s1[] = "1 34";
+	char s2[] = "12'5";
 	// returns 15
 	//int size = sizeof(s1)/sizeof(s1[0])
+	//int arr[] = { 5, 1 , 10, 11, 4 };
+	cout << strcmp_ncase(s1, size(s1) , s2, size(s2), true, true);
 	
-	cout << strcmp_ncase(s1, sz(s1) , s2, sz(s2));
-	
-	
+		
 	
 	return 0;
 }
