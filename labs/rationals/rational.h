31c06e9b849af52ea4a088293c5aa9938d54337a
Oleg Silkin <o.silkin98@gmail.com>
2017-09-17 12:48:37 -0500

    update rational.h
    removed explicit definitions from within header file into cpp file to conform to more of a c++ standard

--- labs/rationals/rational.h
+++ labs/rationals/rational.h
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <iostream>
 
 #ifndef RATIONAL_H
@@ -10,20 +9,9 @@ class rational {
 		int num, dem;
 		double ratio;
 		
-		bool set_dem(const int d) {
-			switch(d) {
-				case 0:
-					return false;
-				default:
-					dem = d;
-					return true;
-			}
-		}
-		
-		bool set_num(const int n) { 
-			num = n; 
-			return true;
-		}
+		/****** CRITICAL MUTATORS *****/
+		bool set_dem(const int d);
+		bool set_num(const int n);
 		
 	public:
 		/****** ACCESSORS *********/
@@ -34,90 +22,49 @@ class rational {
 		
 		/******* CONSTRUCTORS ********/
 		
-		rational(void) { 
-			set_frac(1, 1);
-		}
+		rational(void);
 		
-		rational(const int n, const int d) {
-			if(!set_frac(n, d)) {
-				set_frac(1, 1);
-			}
-		}
+		rational(const int n, const int d);
 		
-		/****** UTILITIES *************/
-		// checks to see if the denominator value != 0 & sets 
-		bool set_frac(const int n, const int d) {
-			
-			if(d < 0 && n < 0) { // if both n & d are negative then factor out the -1
-				set_dem((-1)*d);
-				set_num((-1)*n);
-			}
-			else if(set_dem(d)) { // else if d is non-zero 
-				return set_num(n);
-			} else { // else if d is 0 return nothing; dont set 
-				return false;
-			}
-		}
 		
-		char const* print(void) {
-			//std::cout << num << "/" << dem << std::endl;
-			if( dem < 0 ) {
-				set_frac(-1 * num, -1 * dem);
-			}
-			return  num + "/" + dem;
-		}
+		/****** MUTATORS  *************/
+		bool set_frac(const int n, const int d);
+		bool change_dem(const int d); // must be implemented 
+		bool change_num(const int n);
+		
+		
+		// print function 
+		char const* print(void) { return  num + "/" + dem; }
 		
 		//additive inverse will be added whence I am to receive more clarification from the instructor
 		
-			
 		
-		/******* MATH RELATED FUNCTIONS *******/
 		
-		bool reciprocal(void) {
-			return set_frac(dem, num);
-		}
+		/******* MATH RELATED FUNCTIONS (MUTATORS) *******/
+		
+		bool reciprocal(void);
 		// sum
-		bool sum(rational & other) {
-			return set_frac(num * other.get_dem() + dem * other.get_num(), other.get_dem() * dem);
-		}
+		bool sum(rational & other);
 		
-		bool diff(rational & other) { 
-			return set_frac(num * other.get_dem() - dem * other.get_num(), dem * other.get_dem());
-		}
+		bool diff(rational & other);
 		
-		bool product(rational & other) {
-			return set_frac(num * other.get_num(), dem * other.get_dem());
-		}
+		bool product(rational & other);
 		
-		bool quotient(rational & other) { 
-			return set_frac( num * other.get_dem(), dem * other.get_num());
-		}
+		bool quotient(rational & other);
 		
 		/***** COMPARISON FUNCTIONS ****/
 		
-		bool less_than(rational & other) { 
-			return num * other.get_dem() < dem * other.get_num();
-		}
-		
-		bool greater_than(rational & other) {
-			return num * other.get_dem() > dem * other.get_num();
-		}
-		
-		bool equal_to(rational & other) {
-			return num * other.get_dem() == dem * other.get_num();
-		}
-		
-		bool greater_than_equal_to(rational & other) {
-			return greater_than(other) || equal_to(other);
-		}
-		
-		bool less_than_equal_to(rational & other) {
-			return less_than(other) || equal_to(other);
-		}
-		
-		bool not_equal_to(rational & other) {
-			return !equal_to(other);
-		}
+		bool less_than(rational & other) { return num * other.get_dem() < dem * other.get_num(); }
+
+		bool greater_than(rational & other) { return num * other.get_dem() > dem * other.get_num(); }
+
+		bool equal_to(rational & other) { 	return num * other.get_dem() == dem * other.get_num(); }
+
+		bool greater_than_equal_to(rational & other) { 	return greater_than(other) || equal_to(other); }
+
+		bool less_than_equal_to(rational & other) { return less_than(other) || equal_to(other); }
+
+		bool not_equal_to(rational & other) { return !equal_to(other); }
 };
 
 #endif
