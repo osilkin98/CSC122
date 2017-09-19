91b4417fe56371ca2d2ece292d4de86b5e4887da
Oleg Silkin <o.silkin98@gmail.com>
2017-09-19 16:55:50 -0500

    Update City.h

--- projects/mapitout/City.h
+++ projects/mapitout/City.h
@@ -13,25 +13,29 @@ class City
 	char name[MAX_CITY_NAME];
 public:
 	City(void);
+	// calculates the distance between this city and another
 	double distance(const City & other) const
 	{
 		return location.distance(other.location);
 	}
+	// accessor method to return the city's current location as a point
 	Point get_location(void) const
 	{
 		return location;
 	}
-
+	// returns the city's name as a c-string
 	char* get_name(void) {
 		return name;
 	}
 
 private:
+	// sets a new location for this city
 	void set_location(const Point & new_loc) {
 		location.set_x(new_loc.get_x());
 		location.set_y(new_loc.get_y());
 	}
 public:
+	// basically just creates a new location for the city (poorly designed)
 	void new_location(void) {
 		cout << "Enter the city's location (x-coordinate, y-coordinate): ";
 		location.Input();
@@ -42,13 +46,13 @@ public:
 		cin.ignore();
 		cin.getline(name, MAX_CITY_NAME);
 	}
-	
+	// creates a new entry for the city, again very poorly designed
 	void new_entry(void) {
 		new_location();
 		new_name();
 	}
 	
-	
+	// print the city information out to the screen
 	void print(void) {
 		cout << "\tName: " << name << "\n\tLocation: ( x: " << location.get_x() << " , y: " << location.get_y() << " )\n";
 	}
