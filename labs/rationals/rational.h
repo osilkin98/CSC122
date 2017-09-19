c2bd85d0f08b7cee9c987a300f6508719f5f5556
Oleg Silkin <o.silkin98@gmail.com>
2017-09-19 11:02:55 -0500

    Update rational.h

--- labs/rationals/rational.h
+++ labs/rationals/rational.h
@@ -6,66 +6,69 @@
 
 class rational {
 	private:
-		int num, dem;
+		int long num, dem;
 		//double ratio;
 		
 		/****** CRITICAL MUTATORS *****/
-		bool set_dem(const int d);
-		bool set_num(const int n);
-		void reduce(int &a, int &b);
+		inline bool set_dem(const long int d);
+		inline bool set_num(const long int n);
+		void reduce(int long &a, int long &b);
 	public:
 		/****** ACCESSORS *********/
-		int get_num(void) { return num; }
-		int get_dem(void) { return dem; }
-		double get_ratio(void) { return ((double)num / (double)dem); }
+		inline int long get_num(void) { return num; }
+		inline int long get_dem(void) { return dem; }
+		inline double get_ratio(void) { return ((double)num / (double)dem); }
 		
-		int gcd(int x, int y);
+		long int gcd(long int x,long int y);
 		
 		/******* CONSTRUCTORS ********/
 		
 		rational(void);
 		
-		rational(const int n, const int d);
+		rational(const long int n, const long int d);
 		
 		
 		/****** MUTATORS  *************/
-		bool set_frac(const int n, const int d);
-		bool change_dem(const int d); // must be implemented 
-		bool change_num(const int n);
+		bool set_frac(const long int n, const long int d);
 		
 		 
-		// print function 
-		char const* get_text(void) { return  num + "/" + dem; }
-		void print(void) { std::cout << num <<  "/" << dem << std::endl; }
+		// returns the rational as text 
+		inline char const* get_text(void) { return  num + "/" + dem; }
+		// simply outputs the rational with no endline
+		inline void Output(void) { std::cout << num <<  "/" << dem; }
+		// takes in the user's input without prompting them to enter anything, which is something they should do on their own
+		bool Input(void);
+		
+
 		//additive inverse will be added whence I am to receive more clarification from the instructor
 		
 		
 		
 		/******* MATH RELATED FUNCTIONS (MUTATORS) *******/
 		
-		bool reciprocal(void);
+		inline rational const reciprocal(void);
 		// sum
-		bool sum(rational & other);
+		inline rational const sum(rational & other);
 		
-		bool diff(rational & other);
+		inline rational const diff(rational & other);
 		
-		bool product(rational & other);
+		inline rational const  product(rational & other);
 		
-		bool quotient(rational & other);
+		inline rational const  quotient(rational & other);
 		
 		/***** COMPARISON FUNCTIONS ****/
 		
-		bool less_than(rational & other) { return num * other.get_dem() < dem * other.get_num(); }
+		inline bool less_than(rational & other) { return num * other.get_dem() < dem * other.get_num(); }
 
-		bool greater_than(rational & other) { return num * other.get_dem() > dem * other.get_num(); }
+		inline bool greater_than(rational & other) { return num * other.get_dem() > dem * other.get_num(); }
 
-		bool equal_to(rational & other) { 	return num * other.get_dem() == dem * other.get_num(); }
+		inline bool equal_to(rational & other) { 	return num * other.get_dem() == dem * other.get_num(); }
 
-		bool greater_than_equal_to(rational & other) { 	return greater_than(other) || equal_to(other); }
+		inline bool greater_than_equal_to(rational & other) { 	return greater_than(other) || equal_to(other); }
 
-		bool less_than_equal_to(rational & other) { return less_than(other) || equal_to(other); }
+		inline bool less_than_equal_to(rational & other) { return less_than(other) || equal_to(other); }
 
-		bool not_equal_to(rational & other) { return !equal_to(other); }
+		inline bool not_equal_to(rational & other) { return !equal_to(other); }
 };
 
 #endif
